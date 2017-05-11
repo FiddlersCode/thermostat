2a22e9590505a02b5b490188f9041ff6f87faf97
Paula <pmuldoon@umich.edu>
2017-05-11 15:46:28 +0100

    added alert for sub-10 temp

--- thermo-jquery.js
+++ thermo-jquery.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
   $('#uptemp').click(function( clickEvent ) {
     thermostat.UpTemp();
     $(' #temperature' ).text(thermostat.temperature);
-    alert( thermostat.temperature );
-
   });
 
   $('#downtemp').click(function( clickEvent ) {
+    if ( thermostat.temperature === thermostat.MinTemp) {
+      alert('You cannot go below 10 degrees.')
+    }
+    thermostat.DownTemp();
+    $('#temperature').text(thermostat.temperature);
 
-    alert( 'Temp Down Clicked' );
 
   });
 
